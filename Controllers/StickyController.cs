80721d4cc3b1211da0388c542c29a6b361366cd0
KAMIYA.Atsuteru <a-kamiya@pscsrv.co.jp>
2020-09-15 10:23:42 +0900

    9/15の成果

---
+++ Controllers/StickyController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using StickyWebApi.Controllers.POCOs;
+
+namespace StickyWebApi.Controllers
+{
+    [ApiController]
+    [Route("Sticky")]
+    public class StickyController : ControllerBase
+    {
+        public static ConcurrentDictionary<Guid, Sticky> Stickies { get; } = new ConcurrentDictionary<Guid, Sticky>();
+
+        public static int StikeyNumber { get; set; } = 0;
+
+        [HttpGet()]
+        public IEnumerable<Sticky> GetList()
+        {
+            return Stickies.Values;
+        }
+
+        [HttpGet("{id}")]
+        public Sticky Get(Guid id)
+        {
+            return Stickies[id];
+        }
+
+        [HttpGet("add")]
+        public Sticky AddSticky(string title, string text)
+        {
+            StikeyNumber++;
+            var newSticky = new Sticky()
+            {
+                Id = Guid.NewGuid(),
+                Title = $"#{StikeyNumber} {title}",
+                Text = text,
+            };
+            Stickies.AddOrUpdate(newSticky.Id, (id) => newSticky, (id, sticky) => sticky);
+            return newSticky;
+        }
+    }
+}
